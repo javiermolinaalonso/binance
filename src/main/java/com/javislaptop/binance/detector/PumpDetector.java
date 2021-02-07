387cb7709ee690362b0058ecbb36c64335a8d462
javier <javier.molina.alonso@gmail.com>
2021-02-07 22:43:40 +0100

    meh

--- src/main/java/com/javislaptop/binance/detector/PumpDetector.java
+++ src/main/java/com/javislaptop/binance/detector/PumpDetector.java
@@ -2,9 +2,15 @@ package com.javislaptop.binance.detector;
 
 import com.binance.api.client.BinanceApiRestClient;
 import com.binance.api.client.domain.general.SymbolInfo;
+import com.binance.api.client.domain.market.AggTrade;
+import com.binance.api.client.domain.market.Candlestick;
+import com.binance.api.client.domain.market.CandlestickInterval;
 import com.javislaptop.binance.pumper.UnusualVolumeDetector;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,13 +33,24 @@ public class PumpDetector {
                 .map(SymbolInfo::getSymbol)
                 .collect(Collectors.toList());
 
-        btcPairs.forEach(pair -> {
-            boolean orderBookOverAskOutOfMoney = askOutOfMoneyDetector.isOrderBookOverAskOutOfMoney(pair, 50);
-            if (orderBookOverAskOutOfMoney) {
-                
-                System.out.println(pair);
+        LocalDateTime from = LocalDateTime.of(2021, 2, 3, 20, 50, 0);
+        LocalDateTime to = LocalDateTime.of(2021, 2, 3, 21, 00, 10);
+        List<AggTrade> skybtc = binance.getAggTrades("SKYBTC", null, 100000, from.toInstant(ZoneOffset.UTC).toEpochMilli(), to.toInstant(ZoneOffset.UTC).toEpochMilli());
+
+        skybtc.stream().forEach(trade -> {
+            Instant instant = Instant.ofEpochMilli(trade.getTradeTime());
+            boolean isMaker = trade.isBuyerMaker();
+            if (!isMaker) {
+                System.out.println(String.format("%s. %s First: %s. Last: %s", instant.toString(), trade.getPrice(), trade.getFirstBreakdownTradeId(), trade.getLastBreakdownTradeId()));
             }
         });
+//        btcPairs.forEach(pair -> {
+//            boolean orderBookOverAskOutOfMoney = askOutOfMoneyDetector.isOrderBookOverAskOutOfMoney(pair, 50);
+//            if (orderBookOverAskOutOfMoney) {
+//
+//                System.out.println(pair);
+//            }
+//        });
 
     }
 }
