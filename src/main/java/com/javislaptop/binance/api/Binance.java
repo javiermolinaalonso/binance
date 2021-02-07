071f4816de1896e86d1c565ea865251b48344728
javier <javier.molina.alonso@gmail.com>
2021-02-07 16:37:36 +0100

    added limits and market sniff

---
+++ src/main/java/com/javislaptop/binance/api/Binance.java
@@ -0,0 +1,111 @@
+package com.javislaptop.binance.api;
+
+import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.domain.OrderStatus;
+import com.binance.api.client.domain.TimeInForce;
+import com.binance.api.client.domain.account.NewOrderResponse;
+import com.binance.api.client.domain.account.NewOrderResponseType;
+import com.binance.api.client.domain.account.Order;
+import com.binance.api.client.domain.account.Trade;
+import com.binance.api.client.domain.account.request.CancelOrderRequest;
+import com.binance.api.client.domain.account.request.CancelOrderResponse;
+import com.binance.api.client.domain.account.request.OrderStatusRequest;
+import com.binance.api.client.domain.market.AggTrade;
+import com.binance.api.client.exception.BinanceApiException;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.Optional;
+
+import static com.binance.api.client.domain.account.NewOrder.*;
+import static java.util.Optional.empty;
+
+@Service
+public class Binance {
+
+    private final BinanceApiRestClient binanceApiRestClient;
+    private final BinanceFormatter formatter;
+
+    public Binance(BinanceApiRestClient binanceApiRestClient, BinanceFormatter formatter) {
+        this.binanceApiRestClient = binanceApiRestClient;
+        this.formatter = formatter;
+    }
+
+    public Optional<BigDecimal> getAveragePrice(String symbol) {
+        List<AggTrade> aggTrades = binanceApiRestClient.getAggTrades(symbol);
+        double average = aggTrades.stream()
+                .map(AggTrade::getPrice)
+                .mapToDouble(price -> new BigDecimal(price).doubleValue())
+                .average()
+                .orElse(0d);
+        if (average > 0) {
+            return Optional.of(new BigDecimal(average));
+        } else {
+            return empty();
+        }
+    }
+
+    public Optional<Trade> getTrade(String symbol, Long orderId) {
+        return binanceApiRestClient.getMyTrades(symbol).stream().filter(t -> t.getOrderId().equals(String.valueOf(orderId))).findAny();
+    }
+    public Order buyLimit(String symbol, BigDecimal amount, BigDecimal priceLimit) {
+        String priceStr = formatter.formatPrice(symbol, priceLimit);
+        String amountStr = formatter.formatAmount(symbol, amount.divide(priceLimit, 8, RoundingMode.DOWN));
+        System.out.println(String.format("Placing limit order for %s. Amount %s at price %s", symbol, amountStr, priceStr));
+        String orderId = binanceApiRestClient.newOrder(limitBuy(symbol, TimeInForce.GTC, amountStr, priceStr).newOrderRespType(NewOrderResponseType.FULL)).getClientOrderId();
+
+        OrderStatusRequest orderStatusRequest = new OrderStatusRequest(symbol, orderId);
+        int count = 0;
+        Order order = null;
+        do {
+            try {
+                System.out.println("Market limit order not filled");
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                count += 100;
+                order = binanceApiRestClient.getOrderStatus(orderStatusRequest);
+            } catch (BinanceApiException e) {
+                System.out.println(e.getMessage());
+            }
+
+        } while ((order != null && order.getStatus() != OrderStatus.FILLED) && count < 2000);
+
+        return order;
+    }
+
+    public Order buyMarket(String symbol, BigDecimal amount) {
+        String amountStr = formatter.formatAmount(symbol, amount);
+        NewOrderResponse newOrderResponse = binanceApiRestClient.newOrder(marketBuy(symbol, null).quoteOrderQty(amountStr).newOrderRespType(NewOrderResponseType.FULL));
+        return binanceApiRestClient.getOrderStatus(new OrderStatusRequest(symbol, newOrderResponse.getOrderId()));
+    }
+
+    public NewOrderResponse sellLimit(String symbol, String amount, BigDecimal targetPrice) {
+        String price = formatter.formatPrice(symbol, targetPrice);
+        System.out.println("Selling at target price " + targetPrice);
+        return binanceApiRestClient.newOrder(limitSell(symbol, TimeInForce.GTC, amount, price));
+    }
+
+    public NewOrderResponse sellMarket(String symbol, String amount) {
+        return binanceApiRestClient.newOrder(marketSell(symbol, amount).newOrderRespType(NewOrderResponseType.FULL));
+    }
+
+    public CancelOrderResponse cancelOrder(String symbol, Long orderId) {
+        return binanceApiRestClient.cancelOrder(new CancelOrderRequest(symbol, orderId));
+    }
+
+    public String getAssetBalance(String asset) {
+        return binanceApiRestClient.getAccount().getAssetBalance(asset).getFree();
+    }
+
+    public void printPrices(String symbol, String purchasePricestr) {
+        BigDecimal price = new BigDecimal(binanceApiRestClient.getPrice(symbol).getPrice());
+        BigDecimal purchasePrice = new BigDecimal(purchasePricestr);
+        BigDecimal benefit = price.subtract(purchasePrice).divide(purchasePrice, 8, RoundingMode.DOWN).multiply(BigDecimal.valueOf(100));
+        System.out.println("Expected benefit: " + benefit.toPlainString() + "%");
+    }
+}
---
+++ src/main/java/com/javislaptop/binance/api/BinanceFormatter.java
@@ -0,0 +1,84 @@
+package com.javislaptop.binance.api;
+
+import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.domain.general.ExchangeInfo;
+import com.binance.api.client.domain.general.FilterType;
+import com.binance.api.client.domain.general.SymbolFilter;
+import com.binance.api.client.domain.general.SymbolInfo;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class BinanceFormatter {
+
+    private final Map<Integer, DecimalFormat> formatters;
+    private final BinanceApiRestClient binanceApiRestClient;
+    private ExchangeInfo exchangeInfo;
+
+    public BinanceFormatter(BinanceApiRestClient binanceApiRestClient) {
+        this.binanceApiRestClient = binanceApiRestClient;
+        DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.US);
+        otherSymbols.setDecimalSeparator('.');
+        formatters = Map.of(
+                0, new DecimalFormat("0", otherSymbols),
+                1, new DecimalFormat("0.0", otherSymbols),
+                2, new DecimalFormat("0.00", otherSymbols),
+                3, new DecimalFormat("0.000", otherSymbols),
+                4, new DecimalFormat("0.0000", otherSymbols),
+                5, new DecimalFormat("0.00000", otherSymbols),
+                6, new DecimalFormat("0.000000", otherSymbols),
+                7, new DecimalFormat("0.0000000", otherSymbols),
+                8, new DecimalFormat("0.00000000", otherSymbols)
+        );
+    }
+
+    @PostConstruct
+    public void initialize() {
+        this.exchangeInfo = binanceApiRestClient.getExchangeInfo();
+    }
+
+    public String formatPrice(String symbol, BigDecimal value) {
+        return getPriceFormatter(symbol).format(value);
+    }
+
+    public String formatAmount(String symbol, BigDecimal amount) {
+        return getAmountFormatter(symbol).format(amount);
+    }
+
+    private DecimalFormat getAmountFormatter(String symbol) {
+        return formatters.get(getDecimals(symbol, FilterType.LOT_SIZE, SymbolFilter::getStepSize));
+    }
+
+    private DecimalFormat getPriceFormatter(String symbol) {
+        return formatters.get(getDecimals(symbol, FilterType.PRICE_FILTER, SymbolFilter::getTickSize));
+    }
+
+    private int getDecimals(String symbol, FilterType filterType, Function<SymbolFilter, String> function) {
+        String tickSize = getSymbolInfo(symbol).getFilters().stream().filter(f -> f.getFilterType() == filterType).findAny().map(function).orElse("0.00000001");
+
+        switch (tickSize) {
+            case "1.00000000": return 0;
+            case "0.10000000": return 1;
+            case "0.01000000": return 2;
+            case "0.00100000": return 3;
+            case "0.00010000": return 4;
+            case "0.00001000": return 5;
+            case "0.00000100": return 6;
+            case "0.00000010": return 7;
+            default: return 8;
+
+        }
+    }
+
+    private SymbolInfo getSymbolInfo(String symbol) {
+        return exchangeInfo.getSymbols().parallelStream().filter(x -> x.getSymbol().equals(symbol)).findAny().orElseThrow(()-> new RuntimeException("Pair not available"));
+    }
+}
