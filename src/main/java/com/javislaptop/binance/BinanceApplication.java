3d2c361ced0f4644a27ce6c5be25256286d0cdda
javier <javier.molina.alonso@gmail.com>
2021-02-12 18:43:14 +0100

    Updated

--- src/main/java/com/javislaptop/binance/BinanceApplication.java
+++ src/main/java/com/javislaptop/binance/BinanceApplication.java
@@ -2,12 +2,15 @@ package com.javislaptop.binance;
 
 import com.javislaptop.binance.detector.HistoricalPumpDetector;
 import com.javislaptop.binance.detector.RealtimePumpDetector;
+import com.javislaptop.binance.pumper.PumpNDumper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 @EnableConfigurationProperties(BinanceProperties.class)
@@ -17,10 +20,14 @@ public class BinanceApplication {
         SpringApplication.run(BinanceApplication.class, args);
     }
 
+    private final Scanner scanner;
+    private final PumpNDumper pumpNDumper;
     private final RealtimePumpDetector realtimePumpDetector;
     private final HistoricalPumpDetector historicalPumpDetector;
 
-    public BinanceApplication(RealtimePumpDetector realtimePumpDetector, HistoricalPumpDetector historicalPumpDetector) {
+    public BinanceApplication(Scanner scanner, PumpNDumper pumpNDumper, RealtimePumpDetector realtimePumpDetector, HistoricalPumpDetector historicalPumpDetector) {
+        this.scanner = scanner;
+        this.pumpNDumper = pumpNDumper;
         this.realtimePumpDetector = realtimePumpDetector;
         this.historicalPumpDetector = historicalPumpDetector;
     }
@@ -28,7 +35,21 @@ public class BinanceApplication {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            historicalPumpDetector.enablePumpDetection();
+            try {
+            System.out.println("1. Manual pump");
+            System.out.println("2. Automated pump");
+            System.out.println("9. quit");
+            String option = scanner.nextLine();
+            if (option.equals("1")) {
+                pumpNDumper.execute();
+            } else if (option.equals("2")) {
+                realtimePumpDetector.enablePumpDetection();
+            }
+            } catch (Exception e ){
+                e.printStackTrace();
+            } finally {
+                System.exit(0);
+            }
         };
     }
 
